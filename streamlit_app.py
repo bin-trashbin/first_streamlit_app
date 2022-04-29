fe8f0ddd09fb8a68e213c74ec27fe3ec16eeb7f6
Tom Weng <104530845+bin-trashbin@users.noreply.github.com>
2022-04-29 13:35:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index))
+#streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index))
 
-#streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index),['Avocado','Banana'])
+streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index),['Avocado','Banana'])
 
 streamlit.dataframe(my_fruit_list)

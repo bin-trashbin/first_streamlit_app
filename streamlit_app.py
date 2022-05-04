079b8648432445472f5e67d0d91653ce5e0d1136
Tom Weng <104530845+bin-trashbin@users.noreply.github.com>
2022-05-04 10:22:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,12 +37,12 @@ streamlit.header ('Fruityvice Fruit Advice!')
 try:
      fruit_choice = streamlit.text_input('What fruit would you like information about?')
     
-if not fruit_choice:
-  streamlit.error("please select a fruit to get information.")
-else:
-    fruityvice_response.get("https://fruitvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+     if not fruit_choice:
+          streamlit.error("please select a fruit to get information.")
+     else:
+          fruityvice_response.get("https://fruitvice.com/api/fruit/" + fruit_choice)
+          fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+          streamlit.dataframe(fruityvice_normalized)
     
 except URTLError as e:
     streamlit.error()

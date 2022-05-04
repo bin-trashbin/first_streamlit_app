dfca1c1d9498657bbd3dbba0184368f91362b7f5
Tom Weng <104530845+bin-trashbin@users.noreply.github.com>
2022-05-04 10:47:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,11 +30,6 @@ fruits_to_show= my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-def get_fruityvice_data(this_fruit_choice):
-          fruityvice_requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-          fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-          streamlit.dataframe(fruityvice_normalized)
-          return fruityvice_normalized
 
 #New Section to display fruityvice api response
 streamlit.header ('Fruityvice Fruit Advice!')
@@ -54,8 +49,17 @@ except URLError as e:
 streamlit.write('The user entered', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+def get_fruityvice_data(this_fruit_choice):
+          fruityvice_requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+          fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+          streamlit.dataframe(fruityvice_normalized)
+          return fruityvice_normalized
+
+
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
 
 streamlit.stop()
 

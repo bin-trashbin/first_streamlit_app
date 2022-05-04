add061f2181644081d46f23360190d7f74bf4ede
Tom Weng <104530845+bin-trashbin@users.noreply.github.com>
2022-05-04 12:00:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,7 +86,7 @@ streamlit.write('The user entered', second_fruit_choice)
 #streamlit.stop()
 
 def insert_row_snowflake(new_fruit):
-                    with my_cnx.Cursor() as my_cur:
+                    with my_cnx.cursor() as my_cur:
                               my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
                     return "Thanks for adding " + new_fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')

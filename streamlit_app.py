9db8901c909ecbc199f4e52e4242176def7d4af1
Tom Weng <104530845+bin-trashbin@users.noreply.github.com>
2022-05-04 10:53:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,10 +30,19 @@ fruits_to_show= my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+streamlit.write('The user entered', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+def get_fruityvice_data(this_fruit_choice):
+          fruityvice_response= requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+          fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+          return fruityvice_normalized
+
 
 #New Section to display fruityvice api response
 streamlit.header ('Fruityvice Fruit Advice!')
-
+  
 try:
      fruit_choice = streamlit.text_input('What fruit would you like information about?')
     
@@ -44,18 +53,7 @@ try:
           streamlit.dataframe(back_from_function)
           
 except URLError as e:
-    streamlit.error()
-    
-streamlit.write('The user entered', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-def get_fruityvice_data(this_fruit_choice):
-          fruityvice_requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-          fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-          streamlit.dataframe(fruityvice_normalized)
-          return fruityvice_normalized
-
+    streamlit.error()     
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
